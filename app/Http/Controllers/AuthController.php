18d7dc83ebe84241a329a509012da4b4a41a5df9
Bprakoso <mohbagpra123@gmail.com>
2021-10-25 19:50:32 +0700

    endpoint

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+  public function me() {
+    return ['NIS' => 3103119111,
+        'name' => 'Mohammad Bagus Prakoso',
+        'gender' => 'Male',
+        'phone' => '086803425046',
+        'class' => 'XII RPL 4'];
+  }
+}
